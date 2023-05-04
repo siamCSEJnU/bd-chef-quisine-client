1af5572261f160f36c921eb6ce83e268bff7ffa2
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-04 23:31:05 +0600

    added some content at about and contact page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,7 +4,31 @@ import { Container } from "react-bootstrap";
 const About = () => {
   return (
     <Container>
-      <h3 className="text-center">this is about page</h3>
+      <div className="my-3 w-50 mx-auto fs-5 ">
+        <span className="fs-4 fw-semibold"> Welcome to BD QUISINE,</span> <br />{" "}
+        A platform dedicated to showcasing the rich culinary traditions of
+        Bangladesh. Our website is dedicated to providing you with authentic and
+        delicious Bangladeshi recipes, as well as a glimpse into the lives and
+        experiences of talented Bangladeshi chefs. <br /> <br /> At BD QUISINE,
+        we believe that food is not just about nourishing our bodies, but also
+        about connecting with our cultural roots and traditions. That's why we
+        have carefully curated a collection of traditional and modern
+        Bangladeshi recipes, using fresh and locally sourced ingredients. <br />{" "}
+        <br /> Our team of experienced Bangladeshi chefs is passionate about
+        sharing their knowledge and expertise with you. They bring a wealth of
+        culinary experience to our website, having worked in some of the best
+        restaurants and hotels in Bangladesh. Through our website, you'll have
+        the opportunity to learn from their unique perspectives and techniques,
+        and try your hand at creating your own delicious Bangladeshi dishes.{" "}
+        <br /> <br /> We are committed to creating a community of food lovers
+        and enthusiasts, who share our passion for authentic Bangladeshi
+        cuisine. Whether you're a seasoned cook or a beginner, we invite you to
+        join us on this culinary journey and explore the flavors of Bangladesh.{" "}
+        <br /> <br /> Thank you for visiting{" "}
+        <span className="fw-semibold">BD QUISINE</span>. We hope that our
+        website inspires you to discover and appreciate the diverse and
+        delicious culinary heritage of Bangladesh.
+      </div>
     </Container>
   );
 };
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 
 const Contact = () => {
   return (
     <Container>
-      <h3 className="text-center">this is contact page</h3>
+      <h3 className="text-center text-secondary mt-3">
+        Contact with BD QUSINIE
+      </h3>
+      <Form className="w-50 mx-auto">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label className="fs-5 fw-semibold text-success">
+            Email address
+          </Form.Label>
+          <Form.Control type="email" placeholder="your email" />
+        </Form.Group>
+        <Form.Group
+          className="mb-3 fs-5 fw-semibold text-success"
+          controlId="exampleForm.ControlTextarea1"
+        >
+          <Form.Label>Ask for</Form.Label>
+          <Form.Control as="textarea" placeholder="place your mind" rows={5} />
+        </Form.Group>
+        <button type="button" className="btn btn-success">
+          Submit
+        </button>
+      </Form>
     </Container>
   );
 };
